0cf034a0a78d6decb91b7f6887bb375b588475b6
angeling22 <angeling22@gmail.com>
2020-10-31 18:23:44 +0900

    stopwatch hook 으로 변경

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,3 +1,57 @@
+import React, {useEffect, useState} from 'react';
+
+function Stopwatch() {
+  let tickRef;
+  const [isRunning, setIsRunning] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  const tick = () =>{
+    if (isRunning) {
+      setTimer(timer + 1);
+    }
+  }
+
+  useEffect(() => {
+    tickRef = setInterval(tick, 1000);
+    return () => {
+      clearInterval(tickRef);
+    }
+  }, []);
+
+  const getButton = () => {
+    if (isRunning) {
+      return (
+        <button onClick={() => setIsRunning(!isRunning)}>
+          stop
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={() => setIsRunning(!isRunning)}>
+          start
+        </button>
+      );
+    }
+  }
+
+  return (
+    <div className="stopwatch">
+      <h2>Stopwatch</h2>
+      <span className="stopwatch-time">{timer}</span>
+      {
+        getButton()
+      }
+      <button>Reset</button>
+    </div>
+  );
+}
+
+export default Stopwatch;
+
+
+
+
+/*
 import React from 'react';
 
 class Stopwatch extends React.Component {
@@ -59,3 +113,4 @@ class Stopwatch extends React.Component {
 }
 
 export default Stopwatch;
+*/
